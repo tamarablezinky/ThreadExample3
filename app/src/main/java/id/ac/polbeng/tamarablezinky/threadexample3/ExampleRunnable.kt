d4bd28f0de17818a0076eb5c1a411451e5ab9290
tamarablezinky <tamarablezinky05@gmail.com>
2024-10-26 13:23:44 +0700

    Initial commit

---
+++ app/src/main/java/id/ac/polbeng/tamarablezinky/threadexample3/ExampleRunnable.kt
@@ -0,0 +1,15 @@
+package id.ac.polbeng.tamarablezinky.threadexample3
+import android.util.Log
+class ExampleRunnable(private var seconds: Int) : Runnable {
+    private val TAG = "MainActivity"
+    override fun run() {
+        for (i in 1 until seconds) {
+            Log.d(TAG, "startThread: $i")
+            try {
+                Thread.sleep(1000)
+            } catch (ie: InterruptedException) {
+                ie.printStackTrace()
+            }
+        }
+    }
+}
